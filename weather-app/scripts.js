3329fd1bdb7ea2401bf9a61458fd0bcdff183deb
S <christopher.weston@wsu.edu>
2026-04-10 18:41:27 -0700

    Put in form button, adjusted JS. Will begin implementing API hooks

--- weather-app/scripts.js
+++ weather-app/scripts.js
@@ -1,4 +1,17 @@
 
+// Weather API variables for implementation of said API
+
+const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=London&days=3';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '63b2ac205bmsh1930874e842fa48p19a56djsnf40c0674edcf',
+		'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com',
+		'Content-Type': 'application/json'
+	}
+};
+
+
 
 // core function for JSON retrieval
 
@@ -18,11 +31,21 @@ async function getData(url, options) {
   }
 }
 
+// IP lookup variable and whatnot
 
-// get data sample, testing purposes
+let ipLookupURL = "https://api.ipify.org/?format=json";
+let ipLookupOptions = {}; // empty for now
+
+
 
 getData(url, options).then(function (result) {
     // result code for JSON object
 });
 
+
+getData(ipLookupURL, ipLookupOptions).then(function(result) {
+  console.log("IP Data:", result);
+});
+
 // DOM wait commands
+
