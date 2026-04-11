1688f9b8a1f071f36ba073cd29588c156de99cf4
S <christopher.weston@wsu.edu>
2026-04-10 18:57:45 -0700

    JS for API

--- weather-app/scripts.js
+++ weather-app/scripts.js
@@ -1,8 +1,8 @@
 
 // Weather API variables for implementation of said API
 
-const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=London&days=3';
-const options = {
+const weatherUrl = 'https://weatherapi-com.p.rapidapi.com/forecast.json?days=3&q=London';
+const weatherOptions = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-key': '63b2ac205bmsh1930874e842fa48p19a56djsnf40c0674edcf',
@@ -37,15 +37,12 @@ let ipLookupURL = "https://api.ipify.org/?format=json";
 let ipLookupOptions = {}; // empty for now
 
 
-
-getData(url, options).then(function (result) {
-    // result code for JSON object
-});
-
-
 getData(ipLookupURL, ipLookupOptions).then(function(result) {
-  console.log("IP Data:", result);
+  
+
+getData(weatherUrl, weatherOptions).then(function(weatherResult){
+  console.log(weatherResult);
 });
 
 // DOM wait commands
-
+});
