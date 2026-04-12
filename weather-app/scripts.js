bb034e4e35167d637759c5a20e76fe1aef890d30
S <christopher.weston@wsu.edu>
2026-04-11 19:41:36 -0700

    Implemented city search part 1. Removed IP listing until I can get it functional.

--- weather-app/scripts.js
+++ weather-app/scripts.js
@@ -1,7 +1,5 @@
 
 // Weather API variables for implementation of said API
-
-const weatherUrl = 'https://weatherapi-com.p.rapidapi.com/forecast.json?days=3&q=London';
 const weatherOptions = {
 	method: 'GET',
 	headers: {
@@ -11,6 +9,24 @@ const weatherOptions = {
 	}
 };
 
+function getWeather(city) {
+const weatherUrl = `https://weatherapi-com.p.rapidapi.com/forecast.json?days=3&q=${city}`;
+
+getData(weatherUrl, weatherOptions).then(function(weatherResult){
+  updateWeather(weatherResult);
+});
+}
+
+
+//Button functionality to change cities using the function above. preventDefault stops the page from doing a refresh when the button is submitted.
+
+document.getElementById("search-form").addEventListener("submit", function(event) {
+  event.preventDefault(); 
+
+  const city = document.getElementById("cityInput").value;
+  getWeather(city);
+});
+
 
 //Update weather on passed object via DOM
 function updateWeather(weatherObject) {
@@ -19,7 +35,7 @@ function updateWeather(weatherObject) {
     weatherObject.current.temp_f + "°F";
 
     // Forecast loop. The for (let) essentially tells the function to increment each day by the value indicated starting from 0, so i = 0.
-    // Also, $ is an indicator for adding variables to DOM elements, in this cast i.
+    // Also, $ is an indicator for adding variables to DOM elements, in this case, i.
     for (let i = 0; i <3; i++) {
       const day = weatherObject.forecast.forecastday[i];
 
@@ -30,7 +46,7 @@ function updateWeather(weatherObject) {
       day.day.condition.text;
 
       document.getElementById(`forecastDay${i + 1}Temp`).textContent =
-      day.day.mintemp_f + "°F - " + day.day.maxtemp_f + "°F"
+      day.day.mintemp_f + "°F - " + day.day.maxtemp_f + "°F";
 
       document.getElementById(`forecastDay${i + 1}Wind`).textContent =
       "Wind: " + day.day.maxwind_mph + " mph";
@@ -55,21 +71,11 @@ async function getData(url, options) {
   }
 }
 
-// IP lookup variable and whatnot
 
-let ipLookupURL = "https://api.ipify.org/?format=json";
-let ipLookupOptions = {}; // empty for now
-
-
-getData(ipLookupURL, ipLookupOptions).then(function(result) {
-  
-
-getData(weatherUrl, weatherOptions).then(function(weatherResult){
-  console.log(weatherResult);
-  updateWeather(weatherResult);
-});
+//Default city on the page load
+getWeather("London");
 
 
-});
+
 
 addEventListener("DOMContentLoaded")
